2d97664b31ddcbcffdbba7401fd8db86d2118f85
John Faria <johnwbf@hotmail.com>
2019-06-11 09:10:05 -0300

    Criado verbos http

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Req } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
-  @Get()
+  @Get() // /tasks
   getTasks(): string {
-    return 'tasks controller';
+    return `getTasks`;
+  }
+
+  @Post()
+  createTask(@Body() task): string {
+    console.log(task);
+    return 'Creating a task';
+  }
+
+  @Put()
+  updateTask(): string {
+    return 'Edit a task';
+  }
+
+  @Delete()
+  deleteTask(): string {
+    return 'Delete a task';
   }
 }
