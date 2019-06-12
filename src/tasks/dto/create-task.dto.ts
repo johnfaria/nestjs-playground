19f2b3077fe42a5bc7811ad36c0e1131219f9b63
John Faria <johnwbf@hotmail.com>
2019-06-12 08:52:55 -0300

    Editado PUT, DELETE e adicionado DTO

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,5 @@
+export class TaskDTO {
+    readonly name: string;
+    readonly description: string;
+    readonly done: boolean;
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,25 +1,34 @@
-import { Controller, Get, Post, Put, Delete, Body, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { TaskDTO } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
-  @Get() // /tasks
-  getTasks(): string {
-    return `getTasks`;
+  @Get()
+  getTasks(): TaskDTO {
+    return { name: 'run', description: 'typescrpt', done: true };
   }
 
   @Post()
-  createTask(@Body() task): string {
+  createTask(@Body() task: TaskDTO): string {
     console.log(task);
     return 'Creating a task';
   }
 
-  @Put()
-  updateTask(): string {
+  @Put(':id')
+  updateTask(@Body() task: TaskDTO, @Param('id') id): string {
     return 'Edit a task';
   }
 
-  @Delete()
-  deleteTask(): string {
-    return 'Delete a task';
+  @Delete(':id')
+  deleteTask(@Param('id') id): string {
+    return 'Delete a task with id:' + id;
   }
 }
